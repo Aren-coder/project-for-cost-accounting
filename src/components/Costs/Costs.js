816ed873be2a20279290977dc0570547ed29b950
Aren <aren.sahakyan909@gmail.com>
2023-10-13 09:49:13 +0400

    my-commit

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -1,25 +1,23 @@
 import React, { useState } from "react";
 import "./Costs.css";
-import CostItem from "./CostItem";
 import Card from "../UI/Card";
 import CostsFilter from "./CostsFilter";
+import CostList from "./CostList";
 const Costs = (props) => {
   const [selectedYear, setSelecedtYear] = useState("2021");
   const yearChangeHandler = (year) => {
     setSelecedtYear(year);
   };
 
+  const filteredCosts = props.costs.filter((cost) => {
+    return cost.date.getFullYear().toString() === selectedYear;
+  });
+
   return (
     <div>
       <Card className="costs">
         <CostsFilter year={selectedYear} onChangeYear={yearChangeHandler} />
-        {props.costs.map((cost) => (
-          <CostItem
-            date={cost.date}
-            description={cost.description}
-            amount={cost.amount}
-          />
-        ))}
+        <CostList costs = {filteredCosts} />
       </Card>
     </div>
   );
